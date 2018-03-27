c696777b7973689ddc73b0d494518b12858fa9b7
apple <youhappyming@163.com>
2018-03-27 13:51:40 +0800

    提交用户模块

---
+++ MVVM_Demo-master/MVVM_Demo/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  MVVM_Demo
+//
+//  Created by apple on 2018/3/27.
+//  Copyright © 2018年 gongwenkai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@end
---
+++ MVVM_Demo-master/MVVM_Demo/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  MVVM_Demo
+//
+//  Created by apple on 2018/3/27.
+//  Copyright © 2018年 gongwenkai. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
