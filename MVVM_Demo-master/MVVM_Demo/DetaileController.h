0eabd532d4d215f696b93b997252558005d3f7cb
apple <youhappyming@163.com>
2018-03-27 13:53:22 +0800

    提交详情页模块

---
+++ MVVM_Demo-master/MVVM_Demo/DetaileController.h
@@ -0,0 +1,13 @@
+//
+//  DetaileController.h
+//  MVVM_Demo
+//
+//  Created by apple on 2018/3/27.
+//  Copyright © 2018年 gongwenkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetaileController : UIViewController
+
+@end
