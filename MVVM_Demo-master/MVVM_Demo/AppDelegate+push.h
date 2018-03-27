0e85948dac83a56c671fea7c3f2e1b08a5b4a757
apple <youhappyming@163.com>
2018-03-27 11:35:21 +0800

    推送模块哦

---
+++ MVVM_Demo-master/MVVM_Demo/AppDelegate+push.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate+push.h
+//  MVVM_Demo
+//
+//  Created by apple on 2018/3/27.
+//  Copyright © 2018年 gongwenkai. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate (push)
+- (void)initWith:(NSString *)string;
+
+@end
---
+++ MVVM_Demo-master/MVVM_Demo/AppDelegate+push.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate+push.m
+//  MVVM_Demo
+//
+//  Created by apple on 2018/3/27.
+//  Copyright © 2018年 gongwenkai. All rights reserved.
+//
+
+#import "AppDelegate+push.h"
+
+@implementation AppDelegate (push)
+- (void)initWith:(NSString *)string {
+    
+    
+    
+    
+}
+@end
