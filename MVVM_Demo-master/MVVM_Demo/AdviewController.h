5b59be9cc00d1abfccc5b9a237337eeb9f243625
apple <youhappyming@163.com>
2018-03-27 12:11:05 +0800

    增加广告模块

---
+++ MVVM_Demo-master/MVVM_Demo/AdviewController.h
@@ -0,0 +1,13 @@
+//
+//  AdviewController.h
+//  MVVM_Demo
+//
+//  Created by apple on 2018/3/27.
+//  Copyright © 2018年 gongwenkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AdviewController : UIViewController
+
+@end
---
+++ MVVM_Demo-master/MVVM_Demo/AdviewController.m
@@ -0,0 +1,37 @@
+//
+//  AdviewController.m
+//  MVVM_Demo
+//
+//  Created by apple on 2018/3/27.
+//  Copyright © 2018年 gongwenkai. All rights reserved.
+//
+
+#import "AdviewController.h"
+
+@interface AdviewController ()
+
+@end
+
+@implementation AdviewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
